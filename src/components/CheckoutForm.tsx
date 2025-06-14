d7170d8aa56be6531ba5cd281757f773a960c12b
omar <omarenriquecs@gmail.com>
2025-06-14 14:15:20 -0400

    url

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify';
 import { useRouter } from 'next/navigation';
 import { useAuth } from '@/app/context/AuthContext';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL_LOCAL || 'http://localhost:3001/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api';
 
 const CheckoutForm = () => {
   const stripe = useStripe();
